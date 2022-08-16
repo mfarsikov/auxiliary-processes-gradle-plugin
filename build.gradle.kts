aaf0566a70262dc4827fa390b84b090ff24d25fe
Max Farsikov <farsikovmax@gmail.com>
2022-08-17 00:56:35 +0300

    fix pipeline

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,11 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinJavaToolchain
+
 plugins {
     kotlin("jvm") version "1.7.10"
     `maven-publish`
     `java-gradle-plugin`
     id("com.github.mfarsikov.kewt-versioning") version "1.0.0"
-    id ("com.gradle.plugin-publish") version "1.0.0-rc-1"
+    id("com.gradle.plugin-publish") version "1.0.0-rc-1"
 }
 
 repositories {
@@ -11,14 +13,14 @@ repositories {
 }
 
 dependencies {
-    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
-    implementation ("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("com.squareup.okhttp3:okhttp:4.10.0")
 
-    implementation (gradleApi())
+    implementation(gradleApi())
 
-    testImplementation ("org.jetbrains.kotlin:kotlin-test")
-    testImplementation ("org.jetbrains.kotlin:kotlin-test-junit")
+    testImplementation("org.jetbrains.kotlin:kotlin-test")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
 }
 
 project.group = "io.github.mfarsikov.auxiliary-processes"
@@ -40,3 +42,9 @@ pluginBundle {
 }
 project.ext["gradle.publish.key"] = System.getenv("GRADLE_PUBLISH_KEY")
 project.ext["gradle.publish.secret"] = System.getenv("GRADLE_PUBLISH_SECRET")
+
+kotlin {
+    jvmToolchain {
+        languageVersion.set(JavaLanguageVersion.of(17))
+    }
+}
